5ce356372f91f26e640a842f66ff9f8fc14013fb
Steven B <373152+Stevebel@users.noreply.github.com>
2022-02-01 21:50:55 -0500

    Fix serialization for saving

--- src/renderer/pokemon-editor/nav-bar/SaveButton.tsx
+++ src/renderer/pokemon-editor/nav-bar/SaveButton.tsx
@@ -11,6 +11,8 @@ import {
   Snackbar,
 } from '@mui/material';
 import { Box } from '@mui/system';
+import { cloneDeep, omit } from 'lodash';
+import { toJS } from 'mobx';
 import { observer } from 'mobx-react-lite';
 import { useEffect, useState } from 'react';
 import { IPokemonData } from '../../../common/pokemon-data.interface';
@@ -32,13 +34,14 @@ export const SaveButton = observer(() => {
   const save = () => {
     const pokemon: IPokemonData[] = pokemonStore.pokemon.map((p) => ({
       ...p,
-      species: p.species.map((s) => ({ ...s, pokemon: null })),
+      species: p.species.map((s) => cloneDeep(omit(s, 'pokemon'))),
     }));
 
     const data = {
       pokemon,
       lastNationalDex: pokemon.length - 1,
     };
+    console.log(toJS, data);
     window.electron.ipcRenderer.send('pokemon-source-data', data);
     close();
   };
